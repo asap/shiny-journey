1397e319c30cbc1087a5ac661c1c5448333c5a54
ASAP <alexander.sapountzis@gmail.com>
2016-01-13 09:51:21 -0500

    Initial Commit

---
+++ app/assets/javascripts/todos.js
@@ -0,0 +1,21 @@
+// app/assets/javascripts/todos.js
+
+// $(function (){
+//   $("form").submit (function (e) {
+//     e.preventDefault();
+
+//     var action = $(this).attr('action'),
+//         method = $(this).attr('method'),
+
+//         data = $(this).serializeArray();
+
+//     $.ajax({
+//       method: method,
+//       url: action,
+//       data: data,
+
+//       dataType: 'script'
+//     });
+
+//   });
+// });
